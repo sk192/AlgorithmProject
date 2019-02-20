323a530fdc4aa33eb835273af5810d22a34a3262
Shweta Korulkar <korulkar.shweta7@gmail.com>
2019-02-19 16:23:29 -0800

    quick sort using median of median is executing correctly for all positive and negative numbers

--- algoproject.py
+++ algoproject.py
@@ -8,20 +8,20 @@ def median_quick(q, n):
     sublist = []
     median = []
     pivot = None
-    if len(q) > 1:
+    if len(q) >= 1:
         for i in range(0, n, 5):
             sublist.append(q[i:i + 5])
-        print(sublist)
-        for j in range(len(sublist)):
-            s = sorted(sublist[j])
-            median.append(s[(len(s) // 2)])
-        print("median array is: ")
-        print(median)
+        #print(sublist)
+        for j in sublist:
+            s = sorted(j)
+            if len(s) > 0:
+                median.append(s[(len(s) // 2)])
+        #print("median array is: %s" % (median))
+        #print("array s is:  %s " % (s))
+
         if len(median) <= 5:
             sorted(median)
             pivot = median[len(median) // 2]
-            #print ("pivot: %d" %pivot)
-           #  return pivot
         else:
             sorted(median)
             pivot = median_quick(median, len(median) // 2)
@@ -31,12 +31,12 @@ def median_quick(q, n):
 def quicksort(A, p, r):
 
     if p < r:
-        print("p,r %d %d: " %(p,r))
-        q = median_quick(A,len(A))
-        print("pivot: %d" %(q))
+        #print("p,r %d %d: " %(p,r))
+        q = median_quick(A[p: r+1], len(A))
+        #print("pivot q: %d" %(q))
         par = partition(A, p, r, q)
-        print("i+1 index par: %d" %(par))
-        print("r , p: %d %d" %(r, p))
+        #print("i+1 index par: %d" %(par))
+        #print("r , p: %d %d" %(r, p))
         quicksort(A, p, par - 1)
         quicksort(A, par + 1, r)
 
@@ -47,23 +47,24 @@ def partition(A, l, r, pivot):
         if A[i] == pivot:
             break
     A[i], A[r] = A[r], A[i]
-    print("Inisde partition A: ")
-    print(A)
+    #print("Inisde partition A: %s" % (A))
+    #print(A)
     x = A[r]
-    print("partition x: ")
-    print(x)
+
     i = l - 1
+    #print("i, l: %d %d" % (i,l))
     for j in range(l, r ):
         if A[j] <= x:
             i += 1
             A[i], A[j] = A[j], A[i]
     A[i + 1], A[r] = A[r], A[i + 1]
 
-    print("end of partition A:")
-    print(A)
+    #print("end of partition A:")
+    #print(A)
     return i + 1
 
 if __name__ == '__main__':
+
     quick = []
     n = int(input("enter number of elements: "))
     for i in range(n):
