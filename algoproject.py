3d3908eaea2f7117317bc94f06a1b0f691ac7c17
Shweta Korulkar <korulkar.shweta7@gmail.com>
2019-02-22 16:25:52 -0800

    Done QuickSort using MedianOfMedian

--- algoproject.py
+++ algoproject.py
@@ -1,4 +1,5 @@
 #Algorithm Project
+# QuickSort using median-of-median group 5 as a pivot.
 
 import os, sys
 import copy
@@ -31,12 +32,8 @@ def median_quick(q, n):
 def quicksort(A, p, r):
 
     if p < r:
-        #print("p,r %d %d: " %(p,r))
         q = median_quick(A[p: r+1], len(A))
-        #print("pivot q: %d" %(q))
         par = partition(A, p, r, q)
-        #print("i+1 index par: %d" %(par))
-        #print("r , p: %d %d" %(r, p))
         quicksort(A, p, par - 1)
         quicksort(A, par + 1, r)
 
@@ -47,34 +44,43 @@ def partition(A, l, r, pivot):
         if A[i] == pivot:
             break
     A[i], A[r] = A[r], A[i]
-    #print("Inisde partition A: %s" % (A))
-    #print(A)
     x = A[r]
-
     i = l - 1
-    #print("i, l: %d %d" % (i,l))
-    for j in range(l, r ):
+    for j in range(l, r):
         if A[j] <= x:
             i += 1
             A[i], A[j] = A[j], A[i]
     A[i + 1], A[r] = A[r], A[i + 1]
-
-    #print("end of partition A:")
-    #print(A)
     return i + 1
 
 if __name__ == '__main__':
 
     quick = []
-    n = int(input("enter number of elements: "))
-    for i in range(n):
-        v = input("enter element :")
-        quick.append(int(v))
-    print(quick)
-    quick_copy = copy.deepcopy(quick)
-    quicksort(quick, 0, n-1)
+    count = 0
 
+    theFile = os.path.join(os.path.expanduser('~'), 'PycharmProjects', 'AlgoProject', 'ArrayInput.txt')
+    file = open(theFile, 'r')
+    for val in file.read().split():
+        count += 1
+        quick.append(int(val))
+
+    #print("Quick : %s" % (quick))
+    quick_copy = copy.deepcopy(quick)
+    quicksort(quick, 0, count - 1)
     print("mycode : %s" % (quick))
-    # print(quick_copy)
     quick_copy.sort()
     print("inbuilt : %s" % (quick_copy))
+    file.close()
+
+    ''' n = int(input("enter number of elements: "))
+        for i in range(n):
+            v = input("enter element :")
+            quick.append(int(v))
+        print(quick)
+        quick_copy = copy.deepcopy(quick)
+        quicksort(quick, 0, n-1)
+
+        print("mycode : %s" % (quick))
+        # print(quick_copy)
+        quick_copy.sort()
+        print("inbuilt : %s" % (quick_copy))'''
